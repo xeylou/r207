96e21387bd97d3d29f2173d587092b69c9571556
xeylou <71969414+xeylou@users.noreply.github.com>
2023-02-28 15:14:51 +0000

    array from ses grands-morts

--- firstFunction/tp3.py
+++ firstFunction/tp3.py
@@ -1,11 +1,18 @@
 import sqlite3 as sql
 
-con = sql.connect("magasin_sqlite")
+con = sql.connect("tp3_1")
 cur = con.cursor()
 
-cur.execute("select * from produit")
+cur.execute("select * from cours")
 
 res = cur.fetchall()
 
-print("fin.")
+print("<table cellpaddin='20' border='2'>")
+for ligne in res:  
+    print("<tr>")
+    for elt in ligne:
+        print("<td>", elt, "</td>")
+    print("</tr>")
+print("</table>")
+
 con.close()
